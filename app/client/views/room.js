4101d54b62796d857b676ba33250d818b7698a6c
Loic <loic@thoughtworks.com>
2015-07-04 14:20:20 +0800

    Add number of players in the room and start button

--- app/client/views/room.js
+++ app/client/views/room.js
@@ -1,9 +1,22 @@
+function getRoomUsers(roomId) {
+  return Meteor.users.find({
+    field: {
+      currentRoom: roomId
+    }
+  });
+}
+
 Template.room.helpers({
   roomUsers: function() {
-    return Meteor.users.find({
-      field: {
-        currentRoom: this.room._id
-      }
-    })
+    return getRoomUsers(this.room._id);
+  },
+  roomUsersCount: function() {
+    return getRoomUsers(this.room._id).count();
+  }
+});
+
+Template.room.events({
+  "click #start-button": function(event) {
+    console.log("start");
   }
 });
